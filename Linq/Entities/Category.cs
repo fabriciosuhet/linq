40ab28e8760fe30947cc8e2c4e3403e91ac42ec0
fabriciosuhet <fabriciosuhet@hotmail.com>
2023-11-07 17:36:45 -0300

    - LINQ com Lambda - PARTE 1

---
+++ Linq/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Linq.Entities
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
